a83db1bb9dfaf04d409367f477d30a3399f6c3af
Baejunil <bae110707@naver.com>
2024-12-12 14:30:06 +0900

    12-12 라이브러리 사용법

--- chapter03/index.js
+++ chapter03/index.js
@@ -2,6 +2,6 @@
 import lodash from "lodash"
 
 const arr=[1,1,1,2,2,1,1,4,4,3,2]
-const uniqueArr=lodash.uniqueBy(arr)
+const uniqueArr=lodash.uniqBy(arr)
 
 console.log(uniqueArr)
---
+++ chapter04/ani.js
@@ -0,0 +1,4 @@
+//chapter04/ani.j
+import { animated,useSpring } from "@react-spring/web" 
+
+const fadein=({})
